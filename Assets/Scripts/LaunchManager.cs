625ceb9c921bcfd3a7ed82597c266b541e4e59f4
Lee Kwenwon <highfence@naver.com>
2017-10-20 22:01:01 +0900

    [ADD] MonoSingleton

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchManager : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        Screen.SetResolution(800, 600, false);
+        
+    }
+
+}
